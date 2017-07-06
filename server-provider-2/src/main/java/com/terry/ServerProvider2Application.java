0e895f0b0629c0c96be52ced491cf5bdcc65b372
谈太锐 <2219569347@qq.com>
2017-07-06 17:39:40 +0800

    add service providers

---
+++ server-provider-2/src/main/java/com/terry/ServerProvider2Application.java
@@ -0,0 +1,12 @@
+package com.terry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerProvider2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerProvider2Application.class, args);
+	}
+}
