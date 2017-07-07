61585fad09c8311160e28c9b6af41f112d926255
谈太锐 <2219569347@qq.com>
2017-07-07 14:46:47 +0800

    Hystrix fallback

--- feign-consumer/src/main/java/com/terry/UserController.java
+++ feign-consumer/src/main/java/com/terry/UserController.java
@@ -1,5 +1,6 @@
 package com.terry;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +20,19 @@ public class UserController {
     @Autowired
     UserAPI userAPI;
 
+    @HystrixCommand(fallbackMethod = "fallback")
     @GetMapping("/user")
     BaseResponse getUserinfo() {
         BaseResponse response = userAPI.getUserinfo();
         response.message = "This is from eureka service consumer";
+        return response;
+    }
+
+    private BaseResponse fallback() {
+        BaseResponse response = new BaseResponse();
+        response.code = 299;
+        response.message = "服务容错保护";
+        response.data = "Hystrix服务降级";
         return response;
     }
 
